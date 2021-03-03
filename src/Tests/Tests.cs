8fe1a71c7ec22674b8316261e88cb036530a7372
Simon Cropp <simon.cropp@gmail.com>
2021-03-03 19:42:39 +1100

    refs

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DiffEngine;
 using VerifyTests;
 using VerifyNUnit;
 using NUnit.Framework;
@@ -12,31 +13,6 @@ public class Tests
         VerifyDiffPlex.Initialize();
         VerifierSettings.ModifySerialization(
             settings => settings.IgnoreMember<Exception>(_ => _.StackTrace));
-    }
-
-    [Test]
-    public async Task Simple()
-    {
-        var settings = new VerifySettings();
-        settings.UseMethodName("Foo");
-        settings.DisableDiff();
-        settings.DisableClipboard();
-
-        await Verifier.Verify(
-                @"The
-before
-text", settings)
-            .AutoVerify();
-
-        FileNameBuilder.ClearPrefixList();
-        var exception = Assert.ThrowsAsync<Exception>(() =>
-            Verifier.Verify(
-                @"The
-after
-text",
-                settings));
-        await Verifier.Verify(exception!.Message)
-            .ScrubLinesContaining("DiffEngineTray");
     }
 
     [Test]

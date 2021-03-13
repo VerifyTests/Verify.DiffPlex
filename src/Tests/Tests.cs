9a6ff5c43076ed7ee566800c24b231184cce30d2
Simon Cropp <simon.cropp@gmail.com>
2021-03-13 19:13:14 +1100

    Revert "refs"
    This reverts commit 8fe1a71c7ec22674b8316261e88cb036530a7372.

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using DiffEngine;
 using VerifyTests;
 using VerifyNUnit;
 using NUnit.Framework;
@@ -13,6 +12,31 @@ public class Tests
         VerifyDiffPlex.Initialize();
         VerifierSettings.ModifySerialization(
             settings => settings.IgnoreMember<Exception>(_ => _.StackTrace));
+    }
+
+    [Test]
+    public async Task Simple()
+    {
+        var settings = new VerifySettings();
+        settings.UseMethodName("Foo");
+        settings.DisableDiff();
+        settings.DisableClipboard();
+
+        await Verifier.Verify(
+                @"The
+before
+text", settings)
+            .AutoVerify();
+
+        FileNameBuilder.ClearPrefixList();
+        var exception = Assert.ThrowsAsync<Exception>(() =>
+            Verifier.Verify(
+                @"The
+after
+text",
+                settings));
+        await Verifier.Verify(exception!.Message)
+            .ScrubLinesContaining("DiffEngineTray");
     }
 
     [Test]
